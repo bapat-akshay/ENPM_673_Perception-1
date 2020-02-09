e296eceaa4812889cda1d97f36332855815a39bc
PatanSanaulla <spatankhan@gmail.com>
2020-02-08 18:54:31 -0800

    creating matrix x y
    Creating the matrix x, y based on the data read from the CSV file

--- Code/problem_2.py
+++ Code/problem_2.py
@@ -1,7 +1,24 @@
 import csv
+import numpy as np
 
 datadict = {}
 with open('data_1.csv') as csvfile:
 	data = csv.reader(csvfile, delimiter = ',')
 	for row in data:
 		datadict[row[0]] = row[1]
+
+#Creating a matrix of each type to further compute
+matrix_y = []
+matrix_x = []
+for i in datadict:
+	try:
+		matrix_y.append(float(datadict[i]))
+		matrix_x.append([float(i)*float(i), float(i), 1])
+	except ValueError:
+		#The value found is not a float
+		continue
+
+y = np.array(matrix_y)
+x = np.array(matrix_x)
+
+
