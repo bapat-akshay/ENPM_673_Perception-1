91b78f8de25ac601e23ea352ec3d293da4839b3f
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-02-11 21:30:58 -0500

    added SVD code

--- Test Code/SVD_test_MIT_example.py
+++ Test Code/SVD_test_MIT_example.py
@@ -2,28 +2,16 @@ import numpy as np
 import math
 from numpy import linalg as LA
 
-# First entry in list set as None for more intuitive indexing
-x = [None, 5, 150, 150, 5]
-y = [None, 5, 5, 150, 150]
-xp = [None, 100, 200, 220, 100]
-yp = [None, 100, 80, 80, 200]
+c = np.array([
+(5,5),
+(-1,7)
+])
 
-A = np.array([[-x[1], -y[1], -1, 0, 0, 0, x[1]*xp[1], y[1]*xp[1], xp[1]], 
-	[0, 0, 0, -x[1], -y[1], -1, x[1]*yp[1], y[1]*yp[1], yp[1]], 
-	[-x[2], -y[2], -1, 0, 0, 0, x[2]*xp[2], y[2]*xp[2], xp[2]], 
-	[0, 0, 0, -x[2], -y[2], -1, x[2]*yp[2], y[2]*yp[2], yp[2]], 
-	[-x[3], -y[3], -1, 0, 0, 0, x[3]*xp[3], y[3]*xp[3], xp[3]], 
-	[0, 0, 0, -x[3], -y[3], -1, x[3]*yp[3], y[3]*yp[3], yp[3]], 
-	[-x[4], -y[4], -1, 0, 0, 0, x[4]*xp[4], y[4]*xp[4], xp[4]], 
-	[0, 0, 0, -x[4], -y[4], -1, x[4]*yp[4], y[4]*yp[4], yp[4]]])
-
-print(A)
-
-A_transpose = np.transpose(A)
+c_transpose = np.transpose(c)
 
 # intermediate_matrix = c_transpose * c
 # intermediate_matrix = np.multiply(c_transpose,c)   #does the dot product
-intermediate_matrix = np.matmul(A_transpose,A)
+intermediate_matrix = np.matmul(c_transpose,c)
 
 # print(f'C transpose:\n {c_transpose}')
 
@@ -43,7 +31,7 @@ eigen_square_root = []
 # print(w[1])
 
 for i in range(0,len(w)):
-	eigen_square_root.append(math.sqrt(abs(w[i])))
+	eigen_square_root.append(math.sqrt(w[i]))
 
 # print(eigen_square_root)
 
@@ -54,17 +42,14 @@ Sigma_eigen_value_matrix = np.diag(eigen_square_root)
 
 Sigma_inverse = np.linalg.inv(Sigma_eigen_value_matrix)
 
-U_matrix = np.matmul(A, np.matmul(V_eigen_vector_matrix,Sigma_inverse))
+U_matrix = np.matmul(c, np.matmul(V_eigen_vector_matrix,Sigma_inverse))
 
 print(f'SVD decomposition of C is:\nU: \n {U_matrix}')	
 print(f'Sigma :\n {Sigma_eigen_value_matrix}')	
 print(f'V transpose :\n {V_transpose}')
 
 SVD_check = np.matmul(U_matrix, np.matmul(Sigma_eigen_value_matrix, V_transpose))
-print(f'SVD check matrix:\n {SVD_check}')
-
-# if A==SVD_check:
-# 	print('SVD successful')
+print(SVD_check)
 
 
 
