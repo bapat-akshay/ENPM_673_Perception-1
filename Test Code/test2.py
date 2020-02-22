3735d5139879b5087a4fe30f2771dba745c48512
akshay_bapat <abapat@terpmail.umd.edu>
2020-02-22 01:35:13 -0500

    Working code for contour detection

--- Test Code/test2.py
+++ Test Code/test2.py
@@ -1,33 +1,38 @@
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture('../Project_1_AR_tag_detection/Tag0.mp4')
+
+# This function reads the b/w image and returns the indices of those contours that have
+# at least one child and exactly one parent. This condition translates to the tag outline
+# in the videos.
+def getTagBoundaryContour(hierarchy):
+	indList = []
+	for index, element in enumerate(hierarchy[0]):
+		if (element[2]!=-1) and (element[3]!=-1) and (hierarchy[0][element[3]][3]==-1):
+			indList.append(index)
+
+	return indList
+
+
+cap = cv.VideoCapture('../Project_1_AR_tag_detection/Video_dataset/multipleTags.mp4')
 
 while(cap.isOpened()):
 	ret, frame = cap.read()
 
-	#img = cv.imread(filename)
 	gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
 
-	#gray = np.float32(gray)
-	# dst = cv.cornerHarris(gray,4,15,0.04)
-	# # cornerHarris(gray, blockSize, ksize, k)
-	# # Reducing k increases the number of corners detected
-	# # Reducing blockSize decreases the number of corners detected
-	# # 
-
-	# #result is dilated for marking the corners, not important
-	# dst = cv.dilate(dst,None)
-
-	# # Threshold for an optimal value, it may vary depending on the image.
-	# frame[dst>0.2*dst.max()]=[0,0,255]
-	# # Reducing threshold increases number of corners detected
+	# Thresholding to improve contour detection
 	ret, thresh = cv.threshold(gray, 200, 255, 0)
-	contours, heirarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-	cv.drawContours(frame, contours[1], -1, (0,255,0), 3)
-	#cv.imshow('', thresh)
+	
+	contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+
+	# Draw each contour
+	for index in getTagBoundaryContour(hierarchy):
+		cv.drawContours(frame, contours[index], -1, (0,0,255), 2)
+
 	cv.imshow('dst',frame)
-	#print(contours)
-	if cv.waitKey(): #& 0xff == 27:
+
+	# Runs till the end of the video
+	if cv.waitKey(1) & 0xff == 27:
 	 	break
 	 	cv.destroyAllWindows()
