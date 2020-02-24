c03a2b92eae87dff938040bcad0a428679cf453a
akshay_bapat <abapat@terpmail.umd.edu>
2020-02-24 01:17:24 -0500

    Added feature to superimpose Lena.png

--- Test Code/test2.py
+++ Test Code/test2.py
@@ -2,24 +2,62 @@ import cv2 as cv
 import numpy as np
 
 
+# Generate a new order of corners according to the detected orientation of the tag
+def reorientCorners(corners, orientation):
+	newCorners = np.array([[0,0], [0,0], [0,0], [0,0]],np.float32)
+	if (orientation == 'down'):
+		newCorners[0] = [corners[3][0], corners[3][1]]
+		newCorners[1] = [corners[0][0], corners[0][1]]
+		newCorners[2] = [corners[1][0], corners[1][1]]
+		newCorners[3] = [corners[2][0], corners[2][1]]
+	elif (orientation == 'right'):
+		newCorners[0] = [corners[2][0], corners[2][1]]
+		newCorners[1] = [corners[3][0], corners[3][1]]
+		newCorners[2] = [corners[0][0], corners[0][1]]
+		newCorners[3] = [corners[1][0], corners[1][1]]
+	elif (orientation == 'up'): 
+		newCorners[0] = [corners[1][0], corners[1][1]]
+		newCorners[1] = [corners[2][0], corners[2][1]]
+		newCorners[2] = [corners[3][0], corners[3][1]]
+		newCorners[3] = [corners[0][0], corners[0][1]]
+	else:
+		newCorners[0] = [corners[0][0], corners[0][1]]
+		newCorners[1] = [corners[1][0], corners[1][1]]
+		newCorners[2] = [corners[2][0], corners[2][1]]
+		newCorners[3] = [corners[3][0], corners[3][1]]
+
+	return newCorners
+
+
+# This function returns the 8x8 matrix that encodes the tag data
 def getTagInfoMat(image):
 	(height, width) = image.shape
 	cellHeight = int(np.floor(height/8))
 	cellWidth = int(np.floor(width/8))
-	cellTotal = cellHeight*cellWidth
+	offset = int(cellHeight*0.1)
+	cellTotal = (cellHeight-offset)*(cellWidth-offset)
+	if not cellTotal:
+		cellTotal = 1
 	infoMat = np.zeros((8,8),np.float32)
+	maximum = [0, None, None]
 	for i in range(8):
 		for j in range(8):
 			cellSum = 0
-			for h_iter in range(i*cellHeight, ((i+1)*cellHeight)-1):
-				for w_iter in range(j*cellWidth, ((j+1)*cellWidth)-1):
+			for h_iter in range(i*cellHeight + offset, ((i+1)*cellHeight)-1-offset):
+				for w_iter in range(j*cellWidth+offset, ((j+1)*cellWidth)-1-offset):
 					cellSum += image[h_iter][w_iter]
-			if (cellSum/cellTotal > 150):
+			if ((i == 2) and (j == 2)) or ((i == 2) and (j == 5)) or ((i == 5) and j == 5) or ((i == 5) and (j == 2)):
+				if (cellSum/cellTotal > maximum[0]):
+					maximum = [cellSum/cellTotal, i, j]
+			elif (cellSum/cellTotal > 200):
 				infoMat[i][j] = 1
+
+	infoMat[maximum[1]][maximum[2]] = 1
 
 	return infoMat
 
 
+# Self-explanatory
 def getTagOrientation(infoMat):
 	if infoMat[2][2] == 1:
 		orientation = 'down'
@@ -34,6 +72,7 @@ def getTagOrientation(infoMat):
 
 
 
+# Get tag ID from decoded tag data
 def getTagID(infoMat):
 	if getTagOrientation(infoMat) == 'down':
 		ID = (2**0)*infoMat[4][4] + (2**1)*infoMat[4][3] + (2**2)*infoMat[3][3] + (2**3)*infoMat[3][4]
@@ -60,6 +99,7 @@ def getTagBoundaryContour(hierarchy):
 	return indList
 
 
+# Detect 4 corners of the contour that represents the tag outline
 def getCorners(contour):
 	rect = [[], [], [], []]
 	maxSum = [None, 0]
@@ -98,7 +138,7 @@ def getCorners(contour):
 
 	# Guard condition for when the above method fails. Typically happens when the inclination
 	# of any of the sides is close to +/-45 degrees.
-	if (abs((rect[0][1] - rect[2][1])) < 50):
+	if (abs((rect[0][0] - rect[2][0])) < 50):
 		rect[0] = contour[minX[0]][0]
 		rect[1] = contour[minY[0]][0]
 		rect[2] = contour[maxX[0]][0]
@@ -108,36 +148,40 @@ def getCorners(contour):
 
 
 
-cap = cv.VideoCapture('../Project_1_AR_tag_detection/Video_dataset/Tag2.mp4')
+cap = cv.VideoCapture('../Project_1_AR_tag_detection/Video_dataset/multipleTags.mp4')
+
+lena = cv.imread('../Project_1_AR_tag_detection/reference_images/images/Lena.png')
+(lenaH, lenaW, c) = lena.shape
+lenaCorners = np.array([[0,0], [0,lenaW], [lenaH,lenaW], [lenaH,0]],np.float32)
 
 while(cap.isOpened()):
 	ret, frame = cap.read()
-
 	gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-
-	# Thresholding to improve contour detection
-	ret, thresh = cv.threshold(gray, 200, 255, 0)
-
+	ret, thresh = cv.threshold(gray, 240, 255, 0) # Thresholding to improve contour detection
 	contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 	# Draw each contour
 	for index in getTagBoundaryContour(hierarchy):
-		# cv.drawContours(frame, contours[index], -1, (0,0,255), 2)
 		corners = getCorners(contours[index])
-		# for point in corners:
-		# 	cv.circle(frame,(point[0],point[1]),10,(0,0,255))
-		width = max(np.linalg.norm(corners[0]-corners[1]), np.linalg.norm(corners[2]-corners[3]))
-		height = max(np.linalg.norm(corners[2]-corners[1]), np.linalg.norm(corners[0]-corners[3]))
-		final = np.array([[0,0], [width-1,0],[width-1,height-1],[0,height-1]],np.float32)
+		width = 100
+		height = 100
+		final = np.array([[0,0], [width-1,0],[width-1,height-1],[0,height-1]],np.float32) # the corners for homographic transformation
 		M = cv.getPerspectiveTransform(corners, final)
 		warped = cv.warpPerspective(frame, M, (width, height))
-		cv.imshow('tag',warped)
+		#cv.imshow('tag',warped)
 		infoMat = getTagInfoMat(cv.cvtColor(warped,cv.COLOR_BGR2GRAY))
-		print(getTagID(infoMat))
+		ID = getTagID(infoMat)
+		#x = corners[0][0]
+		#y = corners[0][1]
+		#cv.putText(frame, 'Tag #' + str(ID) + ' orientation: ' + getTagOrientation(infoMat), (x,y), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+		#print(infoMat)
+		newCorners = reorientCorners(corners, getTagOrientation(infoMat))
+		persp = cv.getPerspectiveTransform(newCorners, lenaCorners)
+		newWarped = cv.warpPerspective(lena, persp, (frame.shape[1], frame.shape[0]), dst=frame, flags=cv.WARP_INVERSE_MAP, borderMode=cv.BORDER_TRANSPARENT)
 
-	#cv.imshow('dst',frame)
+	cv.imshow('dst',frame)
 
 	# Runs till the end of the video
-	if cv.waitKey():# & 0xff == 27:
+	if cv.waitKey(100) & 0xff == 27:
 	 	break
 	 	cv.destroyAllWindows()
